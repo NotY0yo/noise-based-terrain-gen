ae2ef74d5f2b853ee49c60a815c67c6ed1451900
Nathan <Nathan.baker303@gmail.com>
2024-10-28 16:47:12 +0000

    added noise folder, starting to populate

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
-# This is a sample Python script.
+from noise import *
+import numpy as np
+import matplotlib.pyplot as plt
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def display_noise_map(noise_map):
+    """
+    Displays the given noise map using matplotlib.
+from .
+    Parameters:
+    - noise_map (numpy.ndarray): A 2D numpy array representing the noise map to be displayed.
 
+    Returns:
+    None.
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+    Raises:
+    None.
+    """
+
+    # Creating a figure and axis for the plot.
+    fig, ax = plt.subplots()
+
+    # Displaying the noise map as an image.
+    ax.imshow(noise_map, cmap='gray', origin='lower')
+
+    # Adding a colorbar to the plot.
+    cbar = plt.colorbar(ax.imshow(noise_map, cmap='gray', origin='lower'))
+    cbar.set_label('Noise Value')
+
+    # Setting the title and labels for the plot.
+    ax.set_title('Perlin Noise Map')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+
+    # Displaying the plot.
+    plt.show()
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # Example usage of the PerlinNoiseMap class:
+
+    # Creating a PerlinNoiseMap instance with custom parameters
+    perlin_map = perlin.PerlinNoiseMap(width=100, height=100, scale=10, octaves=4, persistence=0.5)
+
+    # Generating a noise map
+    noise_map = perlin_map.generate_noise_map()
+
+    # Displaying the noise map
+    display_noise_map(noise_map)
 
 
---
+++ noise/__init__.py
@@ -0,0 +1,48 @@
+from . import perlin
+
+
+
+"""
+from . import constant
+from . import sin
+from . import white
+
+from . import perlin2
+from . import perlin_ridge
+from . import simplex
+from . import simplex2
+from . import simplex_ridge
+from . import worley
+from . import worley_edge
+from . import billow
+from . import cubic
+from . import line
+from . import shift
+from . import frequency
+from . import add
+from . import multiply
+from . import power
+from . import power_curve
+from . import curve
+from . import gradient
+from . import terrace
+from . import advanced_terrace
+from . import invert
+from . import blend
+from . import alpha
+from . import boost
+from . import steps
+from . import abs
+from . import map
+from . import clamp
+from . import threshold
+from . import min
+from . import max
+from . import warp
+from . import erosion
+from . import linear_spline
+from . import cache
+from . import cell
+from . import legacy_temperature
+from . import legacy_moisture
+"""
