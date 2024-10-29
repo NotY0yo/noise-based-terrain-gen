4732c177ef02c8d7b0b586cd96edfd394f91a6e7
Nathan <Nathan.baker303@gmail.com>
2024-10-29 08:41:12 +0000

    started work on noise functions

--- noise/perlin.py
+++ noise/perlin.py
@@ -4,6 +4,9 @@ import numpy as np
 class PerlinNoise:
     def __init__(self):
         pass
+    def get_val_at_cord(self) -> float:
+        pass
+
     def _interpolate(self, a, b, t):
         # Interpolating between the two values using the interpolation factor.
         return a + (b - a) * t
--- noise/white.py
+++ noise/white.py
@@ -2,13 +2,18 @@ import random
 import numpy as np
 
 class WhiteNoise:
-    def __init__(self):
-        self.map_size = [100,100]
+    def __init__(self, max_val:float=1.0, min_val:float=0.0):
+        self.max:float = max_val
+        self.min:float = min_val
 
-    def get_val_at_cord(self, X, Y):
-        pass
+    def get_val_at_cord(self, X, Y) -> float:
+        val = random.uniform(self.min, self.max)
+        return val
 
     @property
-    def map(self):
-        noise_map = np.random.random(size=self.map_size)
+    def map(self, size:tuple = (100,100)):
+        noise_map = np.zeros(size)
+        for row in range(len(noise_map)):
+            for col in range(len(noise_map[row])):
+                noise_map[row, col] = self.get_val_at_cord(row, col)
         return noise_map
